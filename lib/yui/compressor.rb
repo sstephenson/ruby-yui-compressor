f00a23968c76573764e7cbb0f48e8760ca49749c
MrBongiolo <ralfsb2008@gmail.com>
2010-10-28 17:50:35 -0400

    Solving issue #3

--- lib/yui/compressor.rb
+++ lib/yui/compressor.rb
@@ -7,7 +7,6 @@ if ENV['OS'] == 'Windows_NT'
 else
   require "open3"
 end
-require "open3"
 require "stringio"
 
 module YUI #:nodoc:
@@ -29,6 +28,7 @@ module YUI #:nodoc:
     def initialize(options = {}) #:nodoc:
       @options = self.class.default_options.merge(options)
       @command = [path_to_java, "-jar", path_to_jar_file, *(command_option_for_type + command_options)]
+      @command = @command.flatten.join(' ') if ENV['OS'] == 'Windows_NT'
     end
     
     # Compress a stream or string of code with YUI Compressor. (A stream is
